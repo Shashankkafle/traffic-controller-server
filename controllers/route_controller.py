c4fc7395443476e9a33efa5109f24e230aee8ffc
Shashank Kafle <shashank.kaphle@gmail.com>
2025-10-06 14:32:39 +0700

    create active route setter and getter

--- controllers/route_controller.py
+++ controllers/route_controller.py
@@ -3,7 +3,7 @@ import os
 
 
 # move to dotenv later
-ROUTES_FOLDER = 'C:/Users/GIS2025/Q-learning/dqn_server/intersection/'
+ROUTES_FOLDER = 'C:/Users/GIS2025/Q-learning/dqn_server/intersection'
 
 def get_file_list(directory_path):
     try:
@@ -18,7 +18,28 @@ def get_file_list(directory_path):
 async def route_list(request):
     directory_path = ROUTES_FOLDER
     if not os.path.exists(directory_path):
-        return JSONResponse({"error": "Directory does not exist."}, status_code=404)
+        return JSONResponse({"error": "Directory does not exist.(please correctly set the route directory in the server)"}, status_code=404)
     
     files = get_file_list(directory_path)
     return JSONResponse({"files": files})
+
+async def set_active_route(request):
+    directory_path = ROUTES_FOLDER
+    print("request.app.state",request.app.state)
+    if not os.path.exists(directory_path):
+        print("Directory does not exist set")
+        return JSONResponse({"error": "Directory does not exist.(please correctly set the route directory in the server)"}, status_code=404)
+    form = await request.form()
+    route_name =form.get("route_name")
+    if not route_name or route_name not in get_file_list(directory_path):
+        return JSONResponse({"error": "Route file does not exist."}, status_code=404)
+    request.app.state.active_route_path = route_name
+    return JSONResponse({f"message": f"The active route is now set to {route_name}"}, status_code=200)
+
+async def get_active_route(request):
+    directory_path = ROUTES_FOLDER
+    if not os.path.exists(directory_path):
+        print("Directory does not exist get")
+        return JSONResponse({"error": "Directory does not exist.(please correctly set the route directory in the server)"}, status_code=404)
+    route_name =  request.app.state.active_route_path
+    return JSONResponse({"message": f"The active route is {route_name}"}, status_code=200)
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from starlette.routing import Route
 from controllers.simulation_controller import comparision_simulation, training_simulation
-from controllers.route_controller import route_list
+from controllers.route_controller import route_list, set_active_route, get_active_route
 from starlette.responses import JSONResponse
 
 async def healthcheck(request):
@@ -9,6 +9,8 @@ async def healthcheck(request):
 routes = [
     Route("/compare", endpoint=comparision_simulation, methods=["GET"]),
     Route("/all-routes", endpoint=route_list, methods=["GET"]),
+    Route("/active-route", endpoint=get_active_route, methods=["GET"]),
+    Route("/active-route", endpoint=set_active_route, methods=["POST"]),
     Route("/train", endpoint=training_simulation, methods=["POST"]),
     Route("/health-check", endpoint=healthcheck, methods=["POST"]),
 ]
